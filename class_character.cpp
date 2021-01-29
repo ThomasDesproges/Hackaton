0413764a8db04231c6df47e01f7c4bc3817ec3c3
unknown <thomas.desproges@mines-paristech.fr>
2021-01-29 14:57:08 +0100

    message

--- class_character.cpp
+++ class_character.cpp
@@ -13,87 +13,36 @@ void Character::affiche(){
 
 void Character::movement(char entree){ 
     //Méthode permettant de déplacer le personnage d'une case suivant la commande d'entrée.
-    //La méthode déplace le personnage si cela est possible. N'attaque
+    //La méthode déplace le personnage si cela est possible.
     //on tourne dans le sens horaire, on commence en haut.
+    int dx,dy = 0;0;
     if (entree == controls[0]){
-        if (movement_possible(controls[0])){
-            move_up();
-        }    
+        dy = -1;
     }
     if (entree == controls[1]){
-        if (movement_possible(controls[1])){
-            move_right();
-        }
+        dx = 1;
     }
     if (entree == controls[2]){
-        if (movement_possible(controls[2])){
-            move_down();
-        }
+        dy = 1;
     }
     if (entree == controls[3]){
-        if (movement_possible(controls[3])){
-            move_left();
-        }
+        dx = -1;
+    }
+    if ((mvwinch(stdscr,position_y+dy,position_x+dx) != '|') and (mvwinch(stdscr,position_y+dy,position_x+dx) != '-')){
+        wmove(stdscr,position_y,position_x);
+        addch(' ');
+        wmove(stdscr,position_y+dy,position_x+dx);
+        addch('@');
+        position_x = position_x+dx;
+        position_y = position_y+dy;
+        wmove(stdscr,position_y+5,position_x+5);
+        addch('y');
+        wmove(stdscr,position_y+5,position_x+6);
+        addch(mvwinch(stdscr,position_y+dy,position_x+dx));
     }
     return;
 }
 
-bool Character::movement_possible(char entree){
-    int i_up,i_right,i_down,i_left = 0;0;0;0;
-    if (entree == controls[0]){
-        i_up = 1;
-    }
-    if (entree == controls[1]){
-        i_right = 1;
-    }
-    if (entree == controls[2]){
-        i_down = 1;
-    }
-    if (entree == controls[3]){
-        i_left = 1;
-    }
-    int ch = mvwinch(stdscr,position_y+i_down-i_up,position_x+i_right-i_left);
-    if ((ch=='|')or(ch=='-')){
-        return false;
-    }
-    else{
-        return true;
-    }
-    wmove(stdscr, 5,5);
-    addch(ch);
-}
-
-void Character::move_up(){
-    wmove(stdscr,position_y,position_x);
-    addch(' ');
-    wmove(stdscr,position_y-1,position_x);
-    addch('@');
-    position_y--;
-}
-
-void Character::move_right(){
-    wmove(stdscr,position_y,position_x);
-    addch(' ');
-    wmove(stdscr,position_y,position_x+1);
-    addch('@');
-    position_x++;
-}
-
-void Character::move_down(){
-    wmove(stdscr,position_y,position_x);
-    addch(' ');
-    wmove(stdscr,position_y+1,position_x);
-    addch('@');
-    position_y++;
-}
-
-void Character::move_left(){
-    wmove(stdscr,position_y,position_x);
-    addch(' ');
-    wmove(stdscr,position_y,position_x-1);
-    addch('@');
-    position_x--;
-}
 
 void Character::add_gold(int amount_of_gold){
     bourse += amount_of_gold;
